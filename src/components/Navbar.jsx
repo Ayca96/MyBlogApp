83abc642398751f747211927eca6eec1a06b80ee
Ayça Kübra Soruklu <aycakubra1@gmail.com>
2025-03-16 12:37:30 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+
+
+  // const navigation = [
+  //   { name: "About Us", path: "/about" },
+  //   { name: "Blogs", path: "/blogs" },
+  //   { name: "Contact Us", path: "/contact" }
+  // ];
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
+    <h1 className="text-4xl font-bold">🚀 Tailwind CSS Çalışıyor!</h1>
+  </div>
+  );
+};
+
+export default Navbar;
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Register from "../pages/Register";
+import About from "../pages/About";
+import Login from "../pages/Login";
+
+const AppRouter = () => {
+  return (
+    <Router> 
+     <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
